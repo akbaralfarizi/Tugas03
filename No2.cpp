57600019df7d2d72564751737949b86475129656
Akhmad Akbar Alfarizi <32434489+akbaralfarizi@users.noreply.github.com>
2017-11-08 18:27:15 +0800

    menambahkan if & else pada program

--- No2.cpp
+++ No2.cpp
@@ -6,4 +6,19 @@ int main(){
 	char bidang;
 	cout << "Masukan Huruf Awal Jenis Bidang : ";
 	cin >> bidang;
+	
+	if(bidang == 'P' || bidang == 'p'){
+		cout << " Masukan Panjang : ";
+		cin >> p;
+		cout << " Masukan Lebar : ";
+		cin >> l;
+		L=p*l;
+	}
+	else if(bidang == 'S' || bidang == 's'){
+		cout << " Masukan Alas : ";
+		cin >> a;
+		cout << " Masukan Tinggi : ";
+		cin >> t;
+		L=0.5*(a*t);
+	}
 }
